219fb0adead53e6d8e2215c6e7136b91f7a57173
Prajwal Rudresh <prajwal.rudresh@gmail.com>
2025-10-01 21:50:42 -0500

    template file added

---
+++ template.py
@@ -0,0 +1,38 @@
+import os
+from pathlib import Path
+
+project_name ="cnnClassifier"
+
+list_of_files = [
+    f"src/{project_name}/__init__.py",
+    f"src/{project_name}/components/__init__.py",
+    f"src/{project_name}/utils/__init__.py",
+    f"src/{project_name}/config/__init__.py",
+    f"src/{project_name}/config/configuration.py",
+    f"src/{project_name}/pipeline/__init__.py",
+    f"src/{project_name}/entity/__init__.py",
+    f"src/{project_name}/constants/__init__.py",
+    "config/config.yaml",
+    "dvc.yaml",
+    "params.yaml",
+    "requirements.txt",
+    "setup.py",
+    "research/trials.ipynb",
+    "templates/index.html"
+
+]
+
+
+for filepath in list_of_files:
+    filepath = Path(filepath)
+    filedir, filename = os.path.spilt(filepath)
+
+    if filedir !="":
+        os.makedirs(filedir, exist_ok=True)
+    
+    if (not os.path.exists(filepath)) or (os.path.getsize(filepath) ==0):
+        with open(filepath, "w") as f:
+            pass
+
+    else:
+        print(f"File is already present at: {filepath}")
